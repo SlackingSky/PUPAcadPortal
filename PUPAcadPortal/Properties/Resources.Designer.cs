fa038934900b73fe73cfeec0bea85bd964a27ffc
andrei <andrei@LAPTOP-TL2KUL68>
2026-03-14 14:48:34 +0800

    iconss

--- PUPAcadPortal/Properties/Resources.Designer.cs
+++ PUPAcadPortal/Properties/Resources.Designer.cs
@@ -213,6 +213,16 @@ namespace PUPAcadPortal.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap marketing {
+            get {
+                object obj = ResourceManager.GetObject("marketing", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap pup48x48 {
             get {
                 object obj = ResourceManager.GetObject("pup48x48", resourceCulture);
