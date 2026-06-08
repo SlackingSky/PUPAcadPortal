b9a26d1c500fdcc82d02b05190adf677a0c63164
gelo2117 <gelodc12106@gmail.com>
2026-06-08 14:37:05 +0800

    Added functionalities in Edit SchedAdmin

--- PUPAcadPortal/PortalContents/Admin/SubOffering/EditScheduleContentAdmin.cs
+++ PUPAcadPortal/PortalContents/Admin/SubOffering/EditScheduleContentAdmin.cs
@@ -4,37 +4,115 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Text;
+using System.Linq;
 using System.Windows.Forms;
+using PUPAcadPortal.Models;
+using PUPAcadPortal.Services;
+using System.Drawing.Text;
+
 
 namespace PUPAcadPortal.PortalContents.Admin.SubOffering
 {
     public partial class EditScheduleContentAdmin : UserControl
     {
+        private void EditScheduleContentAdmin_Load(object sender, EventArgs e)
+        {
+            LoadDropdowns();
+            GridFilter();
+            _Isloading = false;
+        }
+
+
         public EditScheduleContentAdmin()
         {
             InitializeComponent();
 
-            try { dgvEditSchedule.Rows.Add(30); } catch { }
+            _scheduleService = new ScheduleService();
 
-            string[,] dummyData =
-            {
-                { "COMP 009", "Object Oriented Programming", "3.0", "2.0", "5.0" },
-                { "COMP 010", "Information Management", "3.0", "2.0", "5.0" },
-                { "COMP 012", "Network Administration", "3.0", "2.0", "5.0" }
-            };
+            cmbESYearLevel.SelectedIndexChanged += FilterChanged;
+            cmbESCurrentSem.SelectedIndexChanged += FilterChanged;
+            cmbESRevYear.SelectedIndexChanged += FilterChanged;
 
-            for (int row = 0; row < 3; row++)
-            {
-                for (int col = 0; col < 5; col++)
-                {
-                    dgvEditSchedule.Rows[row].Cells[col].Value = dummyData[row, col];
-                }
-            }
-            // Mark original rows
-            for (int i = 0; i < 3; i++)
-            {
-                dgvEditSchedule.Rows[i].Tag = "original";
-            }
+
+
+        }
+        private readonly ScheduleService _scheduleService;
+
+        private void GridFilter()
+        {
+            int year = (int)cmbESYearLevel.SelectedValue;
+            int sem = (int)cmbESCurrentSem.SelectedValue;
+            int rev = (int)cmbESRevYear.SelectedValue;
+
+            dgvEditSchedule.DataSource =
+                _scheduleService.GetFilteredSchedule(year, sem, rev);
+
+        }
+
+        private void RefreshGrid()
+        {
+            int year = (int)cmbESYearLevel.SelectedValue;
+            int sem = (int)cmbESCurrentSem.SelectedValue;
+            int rev = (int)cmbESRevYear.SelectedValue;
+
+            dgvEditSchedule.DataSource =
+                _scheduleService.GetFilteredSchedule(year, sem, rev);
+        }
+        private void LoadDropdowns()
+        {
+            ESRoom.DataSource = _scheduleService.GetRooms();
+            ESRoom.DisplayMember = "RoomName";
+            ESRoom.ValueMember = "RoomId";
+
+            ESInstructor.DataSource = _scheduleService.GetProfessors();
+            ESInstructor.DisplayMember = "FullName";
+            ESInstructor.ValueMember = "ProfessorId";
+        }
+
+
+
+        private void DuplicateRow()
+        {
+            if (dgvEditSchedule.CurrentRow == null) return;
+
+            string subjectCode = Convert.ToString(dgvEditSchedule.CurrentRow.Cells["SubjectCode"].Value);
+
+            if (string.IsNullOrEmpty(subjectCode)) return;
+
+            _scheduleService.DuplicateOffering(subjectCode);
+
+            GridFilter();
+        }
+
+        private bool _Isloading = true;
+        private void FilterChanged(object sender, EventArgs e)
+        {
+            if (_Isloading) return;
+            GridFilter();
+        }
+        private void dgvEditSchedule_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void cmbESCurrentSem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshGrid();
+        }
+
+        private void cmbESYearLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshGrid();
+        }
+
+        private void cmbESRevYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshGrid();
+        }
+
+        private void DupeRowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DuplicateRow();
         }
     }
 }
